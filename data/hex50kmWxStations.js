cb2d38490e76343ac5c2cae09200703c62451632
Courtney Simonse <courtney.r.elder@gmail.com>
2022-03-03 12:43:41 -0500

    add semicolon

--- data/hex50kmWxStations.js
+++ data/hex50kmWxStations.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:284627c486b4e8a67b256226e29cc710033b51f9cc03f177eaffe676d3da95c2
-size 132426990
+oid sha256:a9fb264bb4da570712f0081e597a4b87e0e01e6172147373ed6cafe8d73ec08d
+size 132426991
